ddbb697ba299f80332165428a8fe2f0e8d8bd399
Gina Lee <ginalee1114@gmail.com>
2018-06-22 00:48:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ function shout(string) {
   return string.toUpperCase();
 }
 
-// shout('hello');
-
 function whisper(string) {
   return string.toLowerCase();
 }
-
-// whisper('HELLO');
 
 function logShout(string) {
   console.log(`${string}`.toUpperCase());
 }
 
 const spy = 'hello';
+
 logShout(spy);
+
 spy == 'HELLO';
 
 function logWhisper(string) {
