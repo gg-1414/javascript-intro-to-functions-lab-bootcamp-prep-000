a76b4301621d328a224e17ef5d625efb5df7632c
Gina Lee <ginalee1114@gmail.com>
2018-06-22 01:14:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,11 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } else if (string.toUpperCase() === string) {
       return "YES INDEED!";
-  } else if (string === 'I love you, Grandma') {
+  } else if (string == 'I love you, Grandma') {
       return "I love you, too.";
   }
 }
+
+
 
 
